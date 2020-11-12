00b829b08147389f3d528b4e076d0016d81acae0
saleh muhammad fasih <63059222+salehfasih@users.noreply.github.com>
2020-11-12 22:49:33 +0500

    Updates

---
+++ precache-manifest.e3989a0bbf0b790141ccc9b945634bac.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c240efca33dc62ccd9e41aabf7b67f",
+    "url": "/olx-Application/index.html"
+  },
+  {
+    "revision": "558c9cd844dd8308d859",
+    "url": "/olx-Application/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f44afe556c7353b91765",
+    "url": "/olx-Application/static/css/main.69d4d46c.chunk.css"
+  },
+  {
+    "revision": "558c9cd844dd8308d859",
+    "url": "/olx-Application/static/js/2.10d03b56.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/olx-Application/static/js/2.10d03b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44afe556c7353b91765",
+    "url": "/olx-Application/static/js/main.288d0a54.chunk.js"
+  },
+  {
+    "revision": "c761e538be56db1c293b",
+    "url": "/olx-Application/static/js/runtime-main.f14416fe.js"
+  },
+  {
+    "revision": "8acf729924e5d4ba9877faef7d1f4dfe",
+    "url": "/olx-Application/static/media/car.8acf7299.jpg"
+  },
+  {
+    "revision": "88ee7285bb54ca964e1f1b4af632898e",
+    "url": "/olx-Application/static/media/img1.88ee7285.jpg"
+  },
+  {
+    "revision": "8815cdaa9f633d04caa559d12007185a",
+    "url": "/olx-Application/static/media/img10.8815cdaa.jpg"
+  },
+  {
+    "revision": "2cd46ebf4bc3310c73258619e4c317a8",
+    "url": "/olx-Application/static/media/img11.2cd46ebf.jpg"
+  },
+  {
+    "revision": "3f55095d1f2e3baebb93b1bbe229492c",
+    "url": "/olx-Application/static/media/img2.3f55095d.jpg"
+  },
+  {
+    "revision": "a9206b83e9c925b2786ae32c474330d4",
+    "url": "/olx-Application/static/media/img3.a9206b83.jpg"
+  },
+  {
+    "revision": "ac3b5b4846b2b4b36731a12fdd4c31ab",
+    "url": "/olx-Application/static/media/img4.ac3b5b48.jpg"
+  },
+  {
+    "revision": "8cd945c673c5d4c415c3f72a75228a0c",
+    "url": "/olx-Application/static/media/img5.8cd945c6.jpg"
+  },
+  {
+    "revision": "04048f38601c46bba68e3164441294b3",
+    "url": "/olx-Application/static/media/img6.04048f38.jpg"
+  },
+  {
+    "revision": "b0b029affc9be91cb09ddcbcc639ef86",
+    "url": "/olx-Application/static/media/img7.b0b029af.jpg"
+  },
+  {
+    "revision": "df1145ef33bfa6031793f016f197e3c6",
+    "url": "/olx-Application/static/media/img8.df1145ef.jpg"
+  },
+  {
+    "revision": "16bb689bcd271961efef6a4de58ff480",
+    "url": "/olx-Application/static/media/img9.16bb689b.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/olx-Application/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68b5dbddb9830029f48cad285ee73cc5",
+    "url": "/olx-Application/static/media/logo.68b5dbdd.webp"
+  },
+  {
+    "revision": "ac63ca4b20e25ee784015fbba90a0f93",
+    "url": "/olx-Application/static/media/olx.ac63ca4b.png"
+  },
+  {
+    "revision": "a90362c629610bb9e546428785fb2d34",
+    "url": "/olx-Application/static/media/slide.a90362c6.png"
+  }
+]);
